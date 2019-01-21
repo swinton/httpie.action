c4c7caaea9693bcd061a1d65cfacbead3693a200
Steve Winton <stevewinton@gmail.com>
2019-01-21 10:16:00 -0600

    Separate headers from body

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,27 @@ set -e
 # Capture output
 output=$( sh -c "pipenv run http $*" )
 
-# TODO
+# Preserve response for consumption by downstream actions
+response="${HOME}/${GITHUB_ACTION}.response"
+echo "$output" > "$response"
+
 # Separate headers from body
+headers="${response}.headers"
+body="${response}.body"
+dest="${headers}"
 
-# Preserve output for consumption by downstream actions
-echo "$output" > "${HOME}/${GITHUB_ACTION}.txt"
+# Read each line from response
+while IFS="" read -r line || [ -n "$line" ]
+do
+  # Write response line to destination file
+  printf '%s\n' "${line}" >> "${dest}"
 
-# Write output to STDOUT
-echo "$output"
+  # Start writing to body when first zero-length string is found.
+  len=$( echo "${line}" | wc -c )
+  if [ $len -eq 1 ]; then
+    dest="${body}"
+  fi
+done < "${response}"
+
+# Write entire response to STDOUT
+cat "${response}"
