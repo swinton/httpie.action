43092c130c2806eeffc7698a1ee8f3c987c355d2
Steve Winton <stevewinton@gmail.com>
2019-01-21 09:27:17 -0600

    Preserve output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
-sh -c "pipenv run http $*"
+# Capture output
+output=$( sh -c "pipenv run http $*" )
+
+# TODO
+# Separate headers from body
+
+# Preserve output for consumption by downstream actions
+echo "$output" > "${HOME}/${GITHUB_ACTION}.txt"
+
+# Write output to STDOUT
+echo "$output"
